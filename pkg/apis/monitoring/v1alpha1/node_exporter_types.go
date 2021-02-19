4996caa852c14feee599e03c341fa8eb203cca8f
Frederic Branczyk <fbranczyk@gmail.com>
2021-02-19 17:07:31 +0100

    Complete node-exporter configuration options

--- pkg/apis/monitoring/v1alpha1/node_exporter_types.go
+++ pkg/apis/monitoring/v1alpha1/node_exporter_types.go
@@ -13,6 +13,21 @@ type NodeExporter struct {
 }
 
 type NodeExporterSpec struct {
-	// Define resources requests and limits for each node-exporter pod.
+	// The version of node-exporter the specified image represent.
+	Version string `json:"version"`
+	// The image of node-exporter to use.
+	Image string `json:"image"`
+	// ListenAddress is the host node-exporter processes will bind to.
+	// +optional
+	// +kubebuilder:default="127.0.0.1"
+	ListenAddress string `json:"listenAddress"`
+	// Port is the port node-exporter processes will bind to.
+	// +optional
+	// +kubebuilder:default=9100
+	Port int `json:"port"`
+	// Resources defines the resources requests and limits for each
+	// node-exporter pod.
+	// +optional
+	// +kubebuilder:default={requests:{cpu:"102m",memory:"180Mi"},limits:{cpu:"250m",memory:"180Mi"}}
 	Resources v1.ResourceRequirements `json:"resources,omitempty"`
 }
